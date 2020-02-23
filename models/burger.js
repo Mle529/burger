3c2f37c9fb0d4165db0edd5805d323326469ed8a
Michael Le <mnle529@gmail.com>
2020-02-22 23:46:39 -0800

    updated model, db, and config files

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+    selecAll: function (cb) {
+        orm.selecAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    insertNew: function (col, vals, cb) {
+        orm.insertNew("burgers", function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+
+};
+
+module.exports = burger;
